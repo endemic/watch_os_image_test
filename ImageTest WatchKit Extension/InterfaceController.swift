879c5a2f688ebbf3775967f273a699d54192c192
Nathan Demick <ndemick@gmail.com>
2016-04-29 11:50:14 -0400

    Initial Commit

---
+++ ImageTest WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ImageTest WatchKit Extension
+//
+//  Created by Demick, Nathan on 4/29/16.
+//  Copyright © 2016 Demick, Nathan. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
